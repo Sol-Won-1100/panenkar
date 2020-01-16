12e5e1a33c1cc1287246c4d11bd2143b839ac7d7
neilcuz <neil.george.currie@gmail.com>
2020-01-16 23:31:46 +0000

    manual_clean_error still persists, have commented out and opened as an issue

--- R/manual_clean_teams.R
+++ R/manual_clean_teams.R
@@ -13,8 +13,8 @@
 
 manual_clean_teams <- function(database){
   
-  database %>%
-    mutate(away_team = if_else(away_team == "Raith", "Raith Rvs", away_team),
-           match_id = str_replace(match_id, "Raith", "Raith Rvs"))
+  database # %>%
+    # mutate(away_team = if_else(away_team == "Raith", "Raith Rvs", away_team),
+    #       match_id = str_replace(match_id, "Raith", "Raith Rvs"))
 }
 
