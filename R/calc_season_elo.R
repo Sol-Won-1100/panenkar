2eb65e7b1b4c0876a8e9c890ed2bec4af799d992
neilcuz <neil.george.currie@gmail.com>
2020-01-26 23:22:00 +0000

    Fixed problem where elos become NaNs because of a change of format to arg_pd in 2015 where no teams were relegated in the prior season so the average calculation for the promoted teams ELOs had nothing to average over. Have replaced with the average of the 3 lowest ELOs from the teams which were in the league in the previous season

--- R/calc_season_elo.R
+++ R/calc_season_elo.R
@@ -8,72 +8,157 @@
 #'
 #' @export
 
-calc_season_elo <- function(results, elo_record){
+calc_competition_elo <- function(.competition_id, results, 
+                                 message_user = FALSE){
   
-  results <- mutate(results, elo_home_pre = NA_real_, elo_away_pre = NA_real_,
-                    elo_home_post = NA_real_, elo_away_post = NA_real_)
+  metadata <- get_metadata(.competition_id)
   
-  for(j in 1:nrow(results)){
+  results_competition <- results %>%
+    select(match_id, competition_id, season_id, match_date, 
+           home_team:match_result_half2, home_prob:away_prob) %>%
+    filter(competition_id == .competition_id) %>%
+    arrange(match_date) 
+  
+  # Need information on leagues which have promotion or relegation. But we only
+  # need to find the teams which were promoted because by definition the ones
+  # relegated must be the other ones. This also solves the problem of teams 
+  # getting relegated down multiple leagues e.g. Rangers because with this 
+  # approach we don't care we just know they are gone.
+  
+  competition_promoted_into <- metadata$competition_promoted_into
+  
+  if(!is.na(competition_promoted_into)){
+    results_competition_promoted <- results %>%
+      filter(competition_id == competition_promoted_into)
+  }
+  
+  all_teams <- unique(c(results_competition$home_team, 
+                        results_competition$away_team))
+  
+  elo_record <- tibble(team = all_teams, elo = 1000)
+  
+  season_ids <- unique(results_competition$season_id)
+  
+  competition_promoted_into <- metadata$competition_promoted_into
+  
+  results_competition_elos <- list() # To store the results
+  
+  for(i in seq_along(season_ids)){
+
+    results_competition_season <- filter(results_competition, 
+                                         season_id == season_ids[i])
     
-    # Exctract match details - don't exctract date into a vector because it
-    # changes the type so we will access it using the $ operator
+    # What to identify which teams have left the league and which have joined
+    # from last season and establish if they were relegated or promoted. This 
+    # will determine starting ELOs for entry to a league
     
-    match <- results %>% slice(j)
-    
-    match_home_team <- match %>% select(home_team) %>% unlist()
-    match_away_team <- match %>% select(away_team) %>% unlist()
-    match_probs_home <- match %>% select(home_prob) %>% unlist()
-    match_probs_draw <- match %>% select(draw_prob) %>% unlist()
-    match_probs_away <- match %>% select(away_prob) %>% unlist()
-    
-    # Lookup teams elos from the record
-    
-    match_elo_home_pre <- elo_record %>% 
-      filter(team == match_home_team) %>%
-      select(elo) %>%
-      unlist()
-    
-    match_elo_away_pre <- elo_record %>% 
-      filter(team == match_away_team) %>%
-      select(elo) %>%
-      unlist()
-    
-    # Update the pre match elos in the season results
-    
-    results <- results %>%
-      mutate(elo_home_pre = if_else(row_number() == j, 
-                                    match_elo_home_pre,
-                                    elo_home_pre),
-             elo_away_pre = if_else(row_number() == j, 
-                                    match_elo_away_pre,
-                                    elo_away_pre))
-    
-    # Calculate the new elos
-    
-    if(is.na(match_probs_home) | is.na(match_probs_draw) | is.na(match_probs_away)){
-      match_elos_post <- c(match_elo_home_pre, match_elo_away_pre)
-    } else {
-      match_elos_post <- update_elo(match_elo_home_pre, match_elo_away_pre, 
-                                    match_probs_home, match_probs_draw, 
-                                    match_probs_away) %>%
-        unlist()
+    if(i > 1){
+      
+      current_teams <- unique(c(results_competition_season$home_team, 
+                                results_competition_season$away_team))
+      previous_teams <- unique(c(results_competition_season_previous$home_team, 
+                                 results_competition_season_previous$away_team))
+      
+      teams_left <- previous_teams[!(previous_teams %in% current_teams)]
+      teams_new <- current_teams[!(current_teams %in% previous_teams)]
+      
+      # If league has no league on record which has promotion then assume it is
+      # a top tier league
+      
+      if(is.na(metadata$competition_promoted_into)){
+        teams_relegated_from <- teams_left
+        teams_promoted_from <- c()
+        
+        teams_promoted_to <- teams_new
+        teams_relegated_to <- c()
+        
+      } else {
+        results_competition_above <- results_competition_promoted %>%
+          filter(competition_id ==  competition_promoted_into,
+                 season_id == season_ids[i]) 
+        
+        teams_in_above_tier <- results_competition_above %>%
+          select(home_team, away_team) %>%
+          unlist() %>%
+          unique()
+        
+        results_competition_above_previous <- results_competition_promoted %>%
+          filter(competition_id == competition_promoted_into,
+                 season_id == season_ids[i - 1])
+        
+        teams_in_above_tier_previous <- results_competition_above_previous %>%
+          select(home_team, away_team) %>%
+          unlist() %>%
+          unique()
+        
+        teams_promoted_from <- teams_left[teams_left %in% teams_in_above_tier]
+        teams_relegated_from <- teams_left[!(teams_left %in% teams_promoted_from)]
+        
+        teams_relegated_to <- teams_new[teams_new %in% teams_in_above_tier_previous]
+        teams_promoted_to <- teams_new[!(teams_new %in% teams_relegated_to)]
+      }
+      
+      if(length(teams_relegated_to) > 0){
+        teams_promoted_from_average_elo <- elo_record %>%
+          filter(team %in% teams_promoted_from) %>%
+          select(elo) %>%
+          unlist() %>%
+          mean()
+        
+        elo_record <- mutate(elo_record, 
+                             elo = if_else(team %in% teams_relegated_to, 
+                                           teams_promoted_from_average_elo,
+                                           elo))
+      }
+      
+      if(length(teams_promoted_to) > 0){
+        
+        # Sometimes if they change up the format of a league there will be no
+        # relegation and only promotion. In that case we need to modify the code
+        # to take the elo rating from the bottom 3 teams average rather than
+        # the relegated teams average
+        
+        if(length(teams_relegated_from) == 0){
+          teams_relegated_from_average_elo <- elo_record %>%
+            filter(team %in% previous_teams) %>%
+            arrange(elo) %>%
+            slice(1:3) %>%
+            select(elo) %>%
+            unlist() %>%
+            mean()
+          
+        } else {
+          teams_relegated_from_average_elo <- elo_record %>%
+            filter(team %in% teams_relegated_from) %>%
+            select(elo) %>%
+            unlist() %>%
+            mean()         
+        }
+        
+        
+        
+        
+        elo_record <- mutate(elo_record, 
+                             elo = if_else(team %in% teams_promoted_to,
+                                           teams_relegated_from_average_elo,
+                                           elo))
+        
+      }
     }
+
+    results_competition_season_elos <- calc_season_elo(results_competition_season, 
+                                                       elo_record)
     
-    # Update the post match elos in the season results and amend elo record
-    
-    results <- results %>%
-      mutate(elo_home_post = if_else(row_number() == j,
-                                     match_elos_post[1],
-                                     elo_home_post),
-             elo_away_post = if_else(row_number() == j,
-                                     match_elos_post[2],
-                                     elo_away_post))
-    elo_record <- elo_record %>%
-      mutate(elo = if_else(team == match_home_team, match_elos_post[1], elo),
-             elo = if_else(team == match_away_team, match_elos_post[2], elo))
-    
+    # Store
+    results_competition_elos[[i]] <- results_competition_season_elos$results
+    elo_record <- results_competition_season_elos$elo_record
+    results_competition_season_previous <- results_competition_season
     
   }
   
-  return(list(results = results, elo_record = elo_record))
+  if(message_user == TRUE){
+    .competition_id %>% paste0(" complete at ", Sys.time()) %>% message()
+  }
+  
+  return(bind_rows(results_competition_elos))
 }
