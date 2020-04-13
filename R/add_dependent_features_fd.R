08aa21649848f856c6e0d442b91c9fb8144e8c5d
neilcuz <neil.george.currie@gmail.com>
2020-04-13 20:52:14 +0100

    added rows argument to add_dependent_features_fd

--- R/add_dependent_features_fd.R
+++ R/add_dependent_features_fd.R
@@ -2,6 +2,6 @@
 # Need to add the functionality in here but adding the skeleton function because
 # can be used in production without this.
 
-add_dependent_features_fd <- function(x){
+add_dependent_features_fd <- function(x, rows){
   return(x)
 }
