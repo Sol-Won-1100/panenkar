c3c340e4fc70908a264b0b19a97e2b8c8c24abeb
neilcuz <neil.george.currie@gmail.com>
2020-04-16 22:35:43 +0100

    added functions related to building poisson models

---
+++ R/build_model_data_poisson.R
@@ -0,0 +1,38 @@
+
+#' @title Build model data poisson
+#'
+#' @description Reformat the results data into an appropriate format for 
+#' modelling using the basic, time-weighted Poisson model
+#'
+#' @param results results database
+#' @param x1 home goals, uses tidy eval so can specify home_goals_half1 etc
+#' @param x2 away goals, uses tidy eval so can specify away_goals_half1 etc
+#' @param current_date what date to calculate the exponential time weight from
+#' @param xi the time weight parameter
+#'       
+#' @export
+
+build_model_data_poisson <- function(results, x1, x2, current_date, 
+                                     xi = 0.0019){
+  
+  x1 <- enquo(x1)
+  x2 <- enquo(x2)
+  
+  model_data_home <- results %>% 
+    select(match_date, home_team, away_team, !!x1) %>%
+    rename(attack = home_team, defence = away_team, goals = !!x1) %>%
+    mutate(location = 1,
+           time_weight = calc_time_weights_exp(match_date, current_date, xi))
+  
+  model_data_away <- results %>% 
+    select(match_date, away_team, home_team, !!x2) %>%
+    rename(attack = away_team, defence = home_team, goals = !!x2) %>%
+    mutate(location = 0,
+           time_weight = model_data_home$time_weight)
+  
+  model_data <- bind_rows(model_data_home, model_data_away) %>%
+    drop_na()
+  
+  return(model_data)
+}
+
---
+++ R/calc_time_weights_exp.R
@@ -0,0 +1,23 @@
+
+#' @title Calculate Time Weights Exponential
+#'
+#' @description Uses an exponetial time weighting for modelling, weighting more
+#' recent matches more strongly. This is derived from the time weighting method
+#' used by Dixon-Coles in their version of the double Poisson model
+#'
+#' @param match_dates dates of football matches, date object
+#' @param current_date what date to calculate the exponential time weight from
+#' @param xi the time weight parameter
+#'       
+#' @export
+
+calc_time_weights_exp <- function(match_dates, current_date, xi){
+  
+  days_difference <- as.numeric(current_date - match_dates)
+  
+  weights <- exp(-xi*days_difference)
+  weights[days_difference <= 0] <- 0
+  
+  return(weights)
+  
+}
